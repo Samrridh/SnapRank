8f0c01f13c6bdca4a888a0ad4a8ce0799b4088ee
Samrridh <samrridh.code@gmail.com>
2025-07-22 00:33:39 +0530

    fixing render hosting error

--- app.py
+++ app.py
@@ -66,4 +66,5 @@ def move(code):
     return {"status": "success"}
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    port = int(os.environ.get("PORT", 10000)) 
+    app.run(host='0.0.0.0', port=port, debug=True, use_reloader=False)
